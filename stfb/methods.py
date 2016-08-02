55c03bab110fb2ece08113e7e75131c598ef9361
Stefano Teso <stefano.teso@gmail.com>
2016-08-02 12:14:01 +0200

    stfb/methods: add debug prints.

--- stfb/methods.py
+++ stfb/methods.py
@@ -57,6 +57,9 @@ def pp(problem, max_iters, targets="attributes", can_critique=False):
     w = np.zeros(problem.num_features, dtype=np.float32)
     w[targets] = np.ones(num_targets) / np.sqrt(num_targets)
 
+    w_star, x_star = problem.w_star, problem.x_star
+    phi_star = problem.phi(x_star, "all")
+
     trace = []
     for it in range(max_iters):
         x = problem.infer(w, targets)
@@ -66,11 +69,35 @@ def pp(problem, max_iters, targets="attributes", can_critique=False):
         else:
             rho, sign = problem.query_critique(x, targets)
 
+        phi = problem.phi(x, "all")
+        print(dedent("""\
+            == ITERATION {it:3d} ==
+
+            w_star =
+            {w_star}
+            w =
+            {w}
+
+            x =
+            {x}
+            x_star =
+            {x_star}
+
+            phi(x) =
+            {phi}
+            phi(x_star) =
+            {phi_star}
+
+            rho = {rho}
+            sign = {sign}
+            """).format(**locals()))
+
         if rho is None:
             x_bar = problem.query_improvement(x, targets)
             w += problem.phi(x_bar, targets) - problem.phi(x, targets)
             is_satisfied = (x == x_bar).all()
         else:
+            x_bar = None
             w[rho] = -sign * problem.get_feature_radius()
             targets.append(rho)
             is_satisfied = False
@@ -79,8 +106,20 @@ def pp(problem, max_iters, targets="attributes", can_critique=False):
         lloss = problem.utility_loss(x, targets)
         gloss = problem.utility_loss(x, "all")
 
-        print("{it:3d} | lloss={lloss} gloss={gloss} |phi|={num_targets}" \
-                  .format(**locals()))
+        print(dedent("""\
+            x_bar =
+            {x_bar}
+
+            new w =
+            {w}
+
+            features = {targets}
+            |features| = {num_targets}
+
+            lloss = {lloss}
+            gloss = {gloss}
+            is_satisfied = {is_satisfied}
+            """).format(**locals()))
 
         trace.append((gloss, -1.0))
         if is_satisfied:
