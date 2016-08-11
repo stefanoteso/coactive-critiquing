f9f43737f9470ab8e959fa9a253658410ee028b5
Stefano Teso <stefano.teso@gmail.com>
2016-08-11 17:58:49 +0200

    datasets/travel: update generation script and data.

--- datasets/make_travel_dataset.py
+++ datasets/make_travel_dataset.py
@@ -3,37 +3,49 @@
 import numpy as np
 import pickle
 
-HORIZON = 10
 NUM_LOCATIONS = 10
 NUM_ACTIVITIES = 5
+MAX_TRAVEL_TIME = 5
 
-rng = np.random.RandomState(0)
+np.random.seed(0)
 
-LOCATION_ACTIVITIES = np.vstack([
-    rng.randint(0, 2, size=(NUM_LOCATIONS, NUM_ACTIVITIES)),
-    np.zeros((1, NUM_ACTIVITIES))
+location_activities = np.vstack([
+    np.random.randint(0, 2, size=(NUM_LOCATIONS, NUM_ACTIVITIES)),
+    np.zeros((1, NUM_ACTIVITIES), dtype=int),
 ])
 
-LOCATION_COST = np.hstack([
-    rng.randint(1, 10, size=NUM_LOCATIONS),
-    [0]
+location_cost = np.hstack([
+    np.random.randint(1, 10, size=NUM_LOCATIONS),
+    [0],
 ])
 
-temp = rng.randint(1, 5, size=(NUM_LOCATIONS, NUM_LOCATIONS))
-TRAVEL_TIME = np.rint((temp + temp.T) / 2)
+half = np.random.randint(1, MAX_TRAVEL_TIME + 1,
+                         size=(NUM_LOCATIONS, NUM_LOCATIONS))
+travel_time = np.rint((half + half.T) / 2).astype(int)
+
+num_regions = np.random.randint(2, NUM_LOCATIONS)
+regions = [np.random.randint(1, num_regions) for _ in range(NUM_LOCATIONS)]
 
 print("""\
 location activities =
-{}
+{location_activities}
 
 location costs =
-{}
+{location_cost}
 
 travel time =
-{}
-""".format(LOCATION_ACTIVITIES, LOCATION_COST, TRAVEL_TIME))
+{travel_time}
 
-DATASET = (LOCATION_ACTIVITIES, LOCATION_COST, TRAVEL_TIME)
+regions =
+{regions}
+""".format(**locals()))
 
+dataset = {
+    "location_activities": location_activities,
+    "location_cost": location_cost,
+    "travel_time": travel_time,
+    "num_regions": num_regions,
+    "regions": regions,
+}
 with open("datasets/travel.pickle", "wb") as fp:
-    pickle.dump(DATASET, fp)
+    pickle.dump(dataset, fp)
