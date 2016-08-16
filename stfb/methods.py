21b0f502032d9a96589af7d14a6e4abc4e784731
Stefano Teso <stefano.teso@gmail.com>
2016-08-16 11:34:35 +0200

    stfb/methods: add back is_satisfied check.

--- stfb/methods.py
+++ stfb/methods.py
@@ -107,7 +107,7 @@ def pp(problem, max_iters, targets="attributes", can_critique=False):
         t1 = time() - t1
 
         rho, sign = None, None
-        if can_critique and not is_separable:
+        if can_critique and not is_satisfied and not is_separable:
             rho, sign = problem.query_critique(x, x_bar, targets)
             assert rho > 0
 
